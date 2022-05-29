02774c62cd4d3497e3a34243f672f3e780bd7c9f
Anton Haldouski <anton94-94@list.ru>
2022-05-29 20:19:24 +0300

    work in class started make function weather and function for add numbers in blocks before

--- JS-task/calendar/calendarJS.js
+++ JS-task/calendar/calendarJS.js
@@ -55,13 +55,21 @@ function addColorForWeekends() {
 function addEmptyBlocksBeforeDate() {
     // Добавляет пустые блоки (в начале каледаря) в календарь если месяц начинается не с понедельника
 
-    let emptyBlockBefore = '<div class="block-date"></div>';
+    let dateBefore = new Date();
+/*    let yearBefore = dateBefore.getFullYear();
+    let monthBefore = dateBefore.getMonth();*/
+    let daysBefore = dateBefore.getDate();
+// Отнять от предыдущего месяца количество пустых блоков и мы получим число а потом это число++
+    console.log(daysBefore)
+    let emptyBlockBefore;
     if (firstDayOfWeek === 0 && firstDayOfWeek !== 1) {
-        for (let j = firstDayOfWeek; j < 6; j++) {
+        for (let j = firstDayOfWeek; j < 6; j++ , daysBefore--) {
+            emptyBlockBefore = `<div class="block-date epmty">${daysBefore}</div>`
             calendarWrapper.insertAdjacentHTML('beforeend', emptyBlockBefore);
         }
     } else if (firstDayOfWeek !== 1) {
-        for (let k = firstDayOfWeek - 1; k > 0; k--) {
+        for (let k = firstDayOfWeek - 1; k > 0; k--, daysBefore--) {
+            emptyBlockBefore = `<div class="block-date epmty">${daysBefore}</div>`
             calendarWrapper.insertAdjacentHTML('beforeend', emptyBlockBefore);
         }
     }
@@ -70,17 +78,19 @@ function addEmptyBlocksBeforeDate() {
 function addEmptyBlocksAfterDate() {
     // Добавляет пустые блоки (в конце каледаря) в календарь
 
-    let emptyBlockAfter = '<div class="block-date"></div>';
+    let emptyBlockAfter;
 
     const blocksAll = document.querySelectorAll('.block-date');
     if (blocksAll.length > 28 && blocksAll.length < 35) {
         let countOfEmptyBlocksAfter = 35 - (blocksAll.length + 1)
         for (let i = 0; i <= countOfEmptyBlocksAfter; i++) {
+            emptyBlockAfter = `<div class="block-date epmty">${i+1}</div>`
             calendarWrapper.insertAdjacentHTML('beforeend', emptyBlockAfter);
         }
     } else if (blocksAll.length > 35 && blocksAll.length < 42) {
         let countOfEmptyBlocksAfter = 42 - (blocksAll.length + 1)
         for (let i = 0; i <= countOfEmptyBlocksAfter; i++) {
+            emptyBlockAfter = `<div class="block-date epmty">${i+1}</div>`
             calendarWrapper.insertAdjacentHTML('beforeend', emptyBlockAfter);
         }
     }
@@ -161,6 +171,21 @@ function markTodayDate() {
 
 }
 
-createCalendar(yearNow, monthNow);
+/*function hoverWeather() {
+    const allBlocksFromCalendar = document.querySelector('.actual-day-in-month');
+    console.log(allBlocksFromCalendar);
 
+    allBlocksFromCalendar.addEventListener('mouseover', ()=>{
+        allBlocksFromCalendar.style.backgroundColor = "red";
+    })
+
+  /!*  (POPUP).addEventListener('mouseleave', ()=>{
+        allBlocksFromCalendar.style.backgroundColor = "blue";
+    })*!/
+}*/
+
+createCalendar(yearNow, monthNow);
+// hoverWeather();
 changeMonth(monthNow);
+
+// Доделать функцию вывода чисел из прошлого месяца!!!!!!!!!!!!!
